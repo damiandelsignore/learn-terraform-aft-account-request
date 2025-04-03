d715c96b38c2c9bdee022fe8078a0668355d8956
Damian <damian_delsignore@shi.com>
2025-04-03 11:18:30 -0500

    adding test-account-4

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,3 +26,33 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+
+module "test_account_4" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "shi_awsprosvcs_testacct4@shi.com"
+    AccountName               = "Test Account 4"
+    ManagedOrganizationalUnit = "Dev (ou-98r4-kvcxa69e)"
+    SSOUserEmail              = "shi_awsprosvcs_testacct4@shi.com"
+    SSOUserFirstName          = "Admin"
+    SSOUserLastName           = "User"
+  }
+
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+    "Account Source" = "Existing"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "legacy"
+}
